5d19ae44dd8b86db1c989c0ab7ed105ce2c3a42e
Carlos Teixeira <carlosmtxcmcmtx@gmail.com>
2014-05-04 15:46:59 +0100

    Done Genetico

---
+++ 2013_2014_IA_AterragensAvioes/Solucao/results.js
@@ -0,0 +1,75 @@
+function drawAll(){
+	var red;
+	var green;
+	var blue;
+	var scaleFactor;
+	function getSortedData(){
+		var json = document.getElementById('jsonData').innerHTML;
+		var data = JSON.parse(json);
+		data.sort(function(a,b){return a.hAterragem - b.hAterragem;});
+		console.log(data);
+		return data;
+	}
+
+	function draw(i,data){
+		var c=document.getElementById('myCanvas');
+		var ctx=c.getContext('2d');
+		ctx.fillStyle= "rgba("+red.toString()+","+green.toString()+","+blue.toString()+",0.5)";
+		
+		red += 100;
+		green = green + red % 256;
+		blue  = blue  + green % 256;
+
+		red = red%256;
+		green=green%256;
+		blue = blue%256; 		
+
+
+		
+		ctx.fillRect(data[i].hAterragem*scaleFactor,30,data[i].tempoOcup*scaleFactor,30);
+		ctx.fillStyle= "rgba(0,0,0,1)";
+		
+
+		while( ctx.measureText(data[i].nome).width > (data[i].tempoOcup*scaleFactor) ){
+			data[i].nome = data[i].nome.substr(0,data[i].nome.length-1);
+		}
+
+
+		ctx.font="10px Georgia";
+		ctx.fillText(data[i].nome,data[i].hAterragem*scaleFactor,50);
+		
+
+		if ( i+1 < data.length){
+			{setTimeout(function(){draw(i+1,data);},1000);}
+		}
+	}
+	var data = getSortedData();
+	red = 0;
+	green = 0;
+	blue = 0;
+
+	
+	var MAXSIZE = 1500;
+	var MAXTIME = data[data.length-1].hAterragem+data[data.length-1].tempoOcup
+
+	scaleFactor = MAXSIZE / MAXTIME;
+	
+	var c=document.getElementById('myCanvas');
+	var ctx=c.getContext('2d');
+	ctx.fillStyle= "rgba(0,0,0,0.1)";
+	ctx.fillRect(0,60,MAXTIME*scaleFactor,30);
+	for ( var i = 0 ; i < MAXTIME;i+=2){
+		ctx.fillStyle= "rgba(0,0,0,0.1)";
+
+		ctx.fillRect(i*scaleFactor,60,scaleFactor,30);
+		ctx.fillStyle= "rgba(0,0,0,1)";		
+		ctx.font="10px Georgia";
+		ctx.fillText(i.toString(),i*scaleFactor,75);
+	}
+
+
+	draw(0,data);
+	
+}
+
+
